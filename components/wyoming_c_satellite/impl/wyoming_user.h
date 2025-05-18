353f079ebbb5b427aaa39e22e760f4cacca5192d
Marek Kraus <gamelaster@outlook.com>
2025-05-18 16:10:57 +0200

    components: Add Wyoming C Satellite and cJSON

---
+++ components/wyoming_c_satellite/impl/wyoming_user.h
@@ -0,0 +1,35 @@
+#ifndef __WYOMING_USER_H_
+#define __WYOMING_USER_H_
+
+#include <ulog/ulog.h>
+
+#include <aos/kernel.h>
+
+// Include required libraries
+#include <cJSON.h>
+
+// Logging implementation
+#include <sys/types.h>
+#include <lwip/sockets.h>
+#include <unistd.h> // For Sockets
+
+#undef LOGE
+#undef LOGD
+#undef LOGI
+#define LOGD(...) ulog(LOG_DEBUG, "wyoming", ULOG_TAG, __VA_ARGS__)
+#define LOGE(...) ulog(LOG_ERR, "wyoming", ULOG_TAG, __VA_ARGS__)
+#define LOGI(...) ulog(LOG_INFO, "wyoming", ULOG_TAG, __VA_ARGS__)
+
+// Platform related macros
+
+#define PLAT_THREAD_TYPE void*
+#define PLAT_THREAD_CREATE(thread, start_routine, name, stack_size, priority) 1
+#define PLAT_THREAD_JOIN(thread) 1
+
+#define PLAT_MUTEX_TYPE aos_mutex_t
+#define PLAT_MUTEX_CREATE(mutex) aos_mutex_new(mutex)
+#define PLAT_MUTEX_DESTROY(mutex) aos_mutex_free(mutex)
+#define PLAT_MUTEX_LOCK(mutex) aos_mutex_lock(mutex, AOS_WAIT_FOREVER)
+#define PLAT_MUTEX_UNLOCK(mutex) aos_mutex_unlock(mutex)
+
+#endif
