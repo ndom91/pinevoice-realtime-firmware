8d7268145b7f66a2d762300c943aa137c5b2fc30
Marek Kraus <gamelaster@outlook.com>
2026-05-11 20:50:16 +0200

    package.sh: Disable debug when packaging release fw

--- package.sh
+++ package.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
+set -e
+set -o pipefail
+
+C906_YAML="solutions/pinevoice_fw_c906/package.yaml"
+E907_YAML="solutions/pinevoice_fw_e907/package.yaml"
+
+restore_yamls() {
+    echo "Restore !!!"
+    if [ -f "$C906_YAML.bak" ]; then
+        rm -f "$C906_YAML"
+        mv "$C906_YAML.bak" "$C906_YAML"
+    fi
+    if [ -f "$E907_YAML.bak" ]; then
+        rm -f "$E907_YAML"
+        mv "$E907_YAML.bak" "$E907_YAML"
+    fi
+}
+trap restore_yamls EXIT
+trap 'echo "package.sh: FAILED at line $LINENO (exit $?)" >&2' ERR
+
+cp "$C906_YAML" "$C906_YAML.bak"
+cp "$E907_YAML" "$E907_YAML.bak"
+
+sed -i -E 's/^([[:space:]]*)(CONFIG_DEBUG[A-Z_]*[[:space:]]*:.*)/\1# \2/' "$C906_YAML"
+sed -i -E 's/^([[:space:]]*)(CONFIG_DEBUG[A-Z_]*[[:space:]]*:.*)/\1# \2/' "$E907_YAML"
 
 pushd solutions/pinevoice_fw_c906
-[ "$1" = "clean" ] && ./go clean
+./go clean
 ./go
 popd
 pushd solutions/pinevoice_fw_e907
-[ "$1" = "clean" ] && ./go clean
+./go clean
 ./go
 popd
+rm -rf .tmp
 mkdir .tmp
 cp boards/bl606p_pinevoice_e907/configs/partition.toml .tmp/partition.toml
 cp boards/bl606p_pinevoice_e907/configs/chip_params.dts .tmp/chip_params.dts
